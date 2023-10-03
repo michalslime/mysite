8457eaa376fe2f7063abd3229a14ca627f31de15
Michał Strzoda <michal.strzoda@gmail.com>
2023-10-03 15:30:33 +0200

    Button for urgent refill added

--- mysite/src/components/refill-component.js
+++ mysite/src/components/refill-component.js
@@ -4,13 +4,19 @@ import binanceService from '../services/api-service';
 import utils from '../services/utils';
 
 const classes = {
-    everyday: ['card']
+    everyday: ['card'],
+    urgentCheckbox: ['card'],
+    urgent: ['card']
 }
 
 function RefillComponent() {
     const [balancePLN, setBalancePLN] = useState(0);
     const [michalBNBBalancePLN, setMichalBNBBalancePLN] = useState(0);
     const [everydayRefillText, setEverydayRefillText] = useState('Uzupełnienie codzienne');
+    const [urgentCheckboxText, setUrgentCheckboxText] = useState('Awaryjnie potrzebuję kasy');
+    const [urgentText, setUrgentText] = useState('Awaryjne uzupełnienie kasy');
+    const [checked, setChecked] = useState(false);
+    const [showUrgentCheckbox, setShowUrgentCheckbox] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -40,12 +46,34 @@ function RefillComponent() {
         return classes[objName].join(' ');
     }
 
+    const handleChange = () => {
+        classes.urgentCheckbox.push('disappear');
+        setUrgentCheckboxText('Prosze czekaj...');
+        setShowUrgentCheckbox(false);
+        setTimeout(() => {
+            setChecked(!checked);
+        }, 10000);
+    };
+
+    const urgentRefill = () => {
+
+    }
+
     return (
         <>
             <div className='card'>Binance card: {balancePLN} PLN</div>
             <div className='card'>MichalBNB: {michalBNBBalancePLN} PLN</div>
             <div className='card' onClick={() => navigate('/')}>Wstecz</div>
             <div className={getClasses('everyday')} onClick={() => everydayRefill()}>{everydayRefillText}</div>
+            {!checked && <div className={getClasses('urgentCheckbox')}>
+                <label>
+                    {showUrgentCheckbox && <input type="checkbox"
+                        checked={checked}
+                        onChange={handleChange} />}{' '}
+                    {urgentCheckboxText}
+                </label>
+            </div>}
+            {checked && <div className={getClasses('urgent')} onClick={() => urgentRefill()}>{urgentText}</div>}
         </>
     )
 }
